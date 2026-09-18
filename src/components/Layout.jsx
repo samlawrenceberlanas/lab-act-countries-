7a7f7bfdf02b1ad7cea4c5a54484848a163b2e8b
berlanas sam lawrence <slberlanas2024@hccci.edu.ph>
2026-09-18 11:13:33 +0800

    Task 1: convert na links to NavLinks with active styling: remove debug borders

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,6 +1,6 @@
 // NOTE: The nav below uses <a href="…"> which triggers full page reloads.
 // Lab 07 asks you to install react-router and replace these with <Link>.
-import { Link, Outlet } from "react-router";
+import { NavLink, Outlet } from "react-router";
 
 const Layout = () => {
   return (
@@ -12,21 +12,32 @@ const Layout = () => {
         </div>
 
         <div className="flex gap-5">
-          <Link to="/">
-            <p className="text-[12px]">Home</p>
-          </Link>
-          <Link to="/countries">
-            <p className="text-[12px]">Countries</p>
-          </Link>
-          <Link to="/bucket-list">
-            <p className="text-[12px]">Bucket List</p>
-          </Link>
-          <Link to="/about">
-            <p className="text-[12px]">About</p>
-          </Link>
+          <NavLink to={"/"} end>
+          {({ isActive }) => (
+            <p className={`text-[12px] ${isActive ? "font-bold text-blue-600" : ""} `}>Home</p>
+          )}
+            </NavLink>
+          
+          <NavLink to="/countries">
+          {({ isActive }) => (
+            <p className={`text-[12px] ${isActive ? "font-bold text-blue-600" : ""} `}>Countries</p>
+          )}
+          </NavLink>
+
+          <NavLink to="/bucket-list">
+          {({ isActive }) => (
+            <p className={`text-[12px] ${isActive ? "font-bold text-blue-600" : ""} `}>Bucket List</p>
+          )}
+          </NavLink>
+
+          <NavLink to="/about">
+          {({ isActive }) => (
+            <p className={`text-[12px] ${isActive ? "font-bold text-blue-600" : ""} `}>About</p>
+          )}
+          </NavLink>
         </div>
       </nav>
-      <main className="flex-1 border-2 border-amber-400 p-3">
+      <main className="flex-1 p-6">
         <Outlet />
       </main>
     </div>
